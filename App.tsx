4092276ce012fa23aa33a0249c1655cccd58ce41
jessejuwe <jessejuwe@gmail.com>
2023-11-03 11:45:48 +0100

    App updated

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import { Linking, Platform } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { PERSISTENCE_KEY } from './helpers/keys';
+import Navigation from './navigators/Navigation';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isReady, setIsReady] = useState(__DEV__ ? false : true);
+  const [initialState, setInitialState] = useState();
+
+  const [fontsLoaded] = useFonts({
+    Lato: require('./assets/fonts/Lato-Regular.ttf'),
+    LatoBold: require('./assets/fonts/Lato-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (Platform.OS !== 'web' && initialUrl == null) {
+          // Only restore state if there's no deep link and we're not on web
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString ? JSON.parse(savedStateString) : undefined; // prettier-ignore
+
+          if (state !== undefined) setInitialState(state);
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) restoreState();
+  }, [isReady]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return <ActivityIndicator />;
+  if (!isReady) return <ActivityIndicator />;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View onLayout={onLayoutRootView} style={styles.container}>
+        <Navigation initialState={initialState} />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({ container: { flex: 1 } });
